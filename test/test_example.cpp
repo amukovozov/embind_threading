309164a0c7836d000bdcf9bc50cff6be8305ace9
a.mukovozov <a.mukovozov@gmail.com>
2021-12-08 18:54:23 +0300

    Add time measurements to the C++ test.

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -1,25 +1,51 @@
 #include <example/example.h>
 
 #include <iostream>
+#include <chrono>
 
 
 int main() {
   constexpr uint64_t n = 1000000000;
 
+  using ClockT = std::chrono::high_resolution_clock;
+  using DurT = ClockT::duration;
+
   Processor processor;
 
   uint32_t result = 0;
+  std::chrono::time_point<ClockT, DurT> t0;
+  std::chrono::time_point<ClockT, DurT> t1;
+  std::chrono::duration<double> dur_s;
 
+  t0 = std::chrono::high_resolution_clock::now();
   result = processor.ProcessNoThreads(n);
-  std::cout << "result: " << result << std::endl;
+  t1 = std::chrono::high_resolution_clock::now();
+  dur_s = t1 - t0;
+  std::cout << "result: " << result << ", time: " << dur_s.count() << std::endl;
+
+  t0 = std::chrono::high_resolution_clock::now();
   result = processor.ProcessThreadsFlat(n);
-  std::cout << "result: " << result << std::endl;
+  t1 = std::chrono::high_resolution_clock::now();
+  dur_s = t1 - t0;
+  std::cout << "result: " << result << ", time: " << dur_s.count() << std::endl;
+
+  t0 = std::chrono::high_resolution_clock::now();
   result = processor.ProcessThreadsNested(n);
-  std::cout << "result: " << result << std::endl;
+  t1 = std::chrono::high_resolution_clock::now();
+  dur_s = t1 - t0;
+  std::cout << "result: " << result << ", time: " << dur_s.count() << std::endl;
+
+  t0 = std::chrono::high_resolution_clock::now();
   result = processor.ProcessTGFlat(n);
-  std::cout << "result: " << result << std::endl;
+  t1 = std::chrono::high_resolution_clock::now();
+  dur_s = t1 - t0;
+  std::cout << "result: " << result << ", time: " << dur_s.count() << std::endl;
+
+  t0 = std::chrono::high_resolution_clock::now();
   result = processor.ProcessTGNested(n);
-  std::cout << "result: " << result << std::endl;
+  t1 = std::chrono::high_resolution_clock::now();
+  dur_s = t1 - t0;
+  std::cout << "result: " << result << ", time: " << dur_s.count() << std::endl;
 
   return 0;
 }
